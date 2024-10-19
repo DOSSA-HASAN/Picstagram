502afe5a6c55fab7e617abe86fdb6f39deeca68f
DOSSA-HASAN <hassandossa5@gmail.com>
2024-10-19 19:39:45 +0300

    navbar fixes

--- src/components/home/UsersHomePage.jsx
+++ src/components/home/UsersHomePage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import './home.css'
 
 function UsersHomePage() {
     return (
-        <>  
+        <div className='section-flex'>  
             <Navbar />
             <section className="users-main-page-section">
-
+                <p>hello</p>
             </section>
-        </>
+        </div>
     )
 }
 
